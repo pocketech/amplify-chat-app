0d5e2b8de3e3568cb97ec0837540efbd690c47d0
tw-nakano <nakano@tailorworks.com>
2021-08-15 12:35:37 +0900

    :test_tube:isrの検証用ページ追加

---
+++ src/pages/isr.tsx
@@ -0,0 +1,19 @@
+import { Text } from '@chakra-ui/react'
+import type { InferGetStaticPropsType, NextPage } from 'next'
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+export const getStaticProps = async () => {
+  const date = new Date()
+  const current = date.toLocaleString()
+  return {
+    props: {
+      current,
+    },
+    revalidate: 5,
+  }
+}
+const Page: NextPage<Props> = ({ current }) => {
+  return <Text>現在時刻は{current}です。</Text>
+}
+export default Page
